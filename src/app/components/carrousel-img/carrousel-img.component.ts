e4ebcb42de99e743efecae2855c77ed89e343d15
Isaac Berrio <berriisaax18@hotmail.com>
2024-07-26 12:40:38 -0500

    Creating the main components

---
+++ src/app/components/carrousel-img/carrousel-img.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrousel-img',
+  standalone: true,
+  imports: [],
+  templateUrl: './carrousel-img.component.html',
+  styleUrl: './carrousel-img.component.scss'
+})
+export class CarrouselImgComponent {
+
+}
---
+++ src/app/components/questions-section/questions-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-questions-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './questions-section.component.html',
+  styleUrl: './questions-section.component.scss'
+})
+export class QuestionsSectionComponent {
+
+}
