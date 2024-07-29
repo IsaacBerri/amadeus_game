bc9ca0f125a99ec77030e6d4cfdfdbca81a83276
Isaac Berrio <berriisaax18@hotmail.com>
2024-07-28 22:57:56 -0500

    Way #1 to navigate between form questions

--- src/app/components/questions-section/questions-section.component.ts
+++ src/app/components/questions-section/questions-section.component.ts
@@ -20,6 +20,52 @@ export class QuestionsSectionComponent {
     }
 
     console.log(option);
-    
+  }
+
+  questionController = 0
+
+  public questions = [
+    {
+      questionText: 'Pick a destination type',
+      questionHero: 'Choose the option that best describes your ideal travel destination',
+      options: [
+        { value: 'City' },
+        { value: 'Beach' },
+        { value: 'Mountain' },
+        { value: 'Countryside' },
+      ],
+    },
+    {
+      questionText: 'What type of transport do you want to travel on',
+      questionHero: 'Choose the option that best describes your ideal travel destination',
+      options: [
+        { value: 'Car' },
+        { value: 'Bus' },
+        { value: 'Train' },
+        { value: 'Plane' },
+      ],
+    },
+    {
+      questionText: 'How long do you plan to stay',
+      questionHero: 'Choose the option that best describes your ideal travel destination',
+      options: [
+        { value: '1 day' },
+        { value: '2 days' },
+        { value: '3 days' },
+        { value: '4 days' },
+      ],
+    }
+  ]
+
+  nextQuestion = () => {
+    if (this.questionController < this.questions.length - 1) {
+      this.questionController++
+    }
+  }
+
+  previousQuestion = () => {
+    if (this.questionController > 0) {
+      this.questionController--
+    }
   }
 }
