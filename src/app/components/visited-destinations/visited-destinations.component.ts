5aad429bfddd5881c825fdbc775503cc9a4b34c5
David Alvarez <david.alvarez321@hotmail.com>
2024-08-19 07:44:41 -0500

    Creation of container with images of most visited places

---
+++ src/app/components/visited-destinations/visited-destinations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visited-destinations',
+  standalone: true,
+  imports: [],
+  templateUrl: './visited-destinations.component.html',
+  styleUrl: './visited-destinations.component.scss'
+})
+export class VisitedDestinationsComponent {
+
+}
