6d10ec563479be1392de721e0365f19125eb12b4
Isaac Berrio <berriisaax18@hotmail.com>
2024-08-20 15:52:14 -0500

    mplementing a first version of the design for the destinations component

--- src/app/pages/destinations/destinations.component.ts
+++ src/app/pages/destinations/destinations.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DestinationComponent } from '@components/destination/destination.component';
+import { QuestionsControllerService } from '@services/questions-controller.service';
 
 @Component({
   selector: 'app-destinations',
   standalone: true,
-  imports: [],
+  imports: [DestinationComponent],
   templateUrl: './destinations.component.html',
   styleUrl: './destinations.component.scss'
 })
 export default class DestinationsComponent {
-
+  responseClient = inject(QuestionsControllerService).responseClient
 }
