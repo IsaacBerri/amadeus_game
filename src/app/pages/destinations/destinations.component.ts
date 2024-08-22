0663be6c75b9448c858c5048ed6d9f722505d6d0
Isaac Berrio <berriisaax18@hotmail.com>
2024-08-22 11:31:51 -0500

    Adding text to destination cards

--- src/app/pages/destinations/destinations.component.ts
+++ src/app/pages/destinations/destinations.component.ts
@@ -13,9 +13,13 @@ export default class DestinationsComponent {
   public destinations = JSON.parse(localStorage.getItem('destinations') || '');
 
   imgsDestinations = {
-    americaDestiny: "",
-    europaDestiny: "",
-  }
+    americaDestiny: '',
+    europaDestiny: '',
+  };
+
+  text = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio tenetur suscipit,
+  soluta fugiat consequatur sint laborum ducimus incidunt quos molestias error saepe. Tenetur
+  quasi veniam deleniti nulla ad! Voluptatum, repellat?`;
 
   API_KEY = '41301808-e86fa23811faafdafbfc9238a';
   categories = 'places, city, destinations';
@@ -24,15 +28,19 @@ export default class DestinationsComponent {
   }
 
   getImgsDestinations() {
-    this.httpClient.get(
-      `https://pixabay.com/api/?key=${this.API_KEY}&q=${this.destinations[0].posibleDestinoEuropa}&image_type=photo&category=${this.categories}`
-    ).subscribe((data: any) => {
-      this.imgsDestinations.europaDestiny = data.hits[0].webformatURL;
-    })
-    this.httpClient.get(
-      `https://pixabay.com/api/?key=${this.API_KEY}&q=${this.destinations[0].posibleDestinoAmerica}&image_type=photo&category=${this.categories}`
-    ).subscribe((data: any) => {
-      this.imgsDestinations.americaDestiny = data.hits[0].webformatURL;
-    })
+    this.httpClient
+      .get(
+        `https://pixabay.com/api/?key=${this.API_KEY}&q=${this.destinations[0].posibleDestinoEuropa}&image_type=photo&category=${this.categories}`
+      )
+      .subscribe((data: any) => {
+        this.imgsDestinations.europaDestiny = data.hits[0].webformatURL;
+      });
+    this.httpClient
+      .get(
+        `https://pixabay.com/api/?key=${this.API_KEY}&q=${this.destinations[0].posibleDestinoAmerica}&image_type=photo&category=${this.categories}`
+      )
+      .subscribe((data: any) => {
+        this.imgsDestinations.americaDestiny = data.hits[0].webformatURL;
+      });
   }
 }
