412fa23a953543c1340746a3d53d01eab72fdde7
Camila Morales <mariakmv@hotmail.com>
2024-08-22 20:59:55 -0500

    video

--- src/app/pages/explore/explore.component.ts
+++ src/app/pages/explore/explore.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderExploreComponent } from '@components/header-explore/header-explore.component';
+import { SectionVideoComponent } from "@components/section-video/section-video.component";
 
 @Component({
   selector: 'app-explore',
   standalone: true,
-  imports: [HeaderExploreComponent],
+  imports: [HeaderExploreComponent, SectionVideoComponent],
   templateUrl: './explore.component.html',
   styleUrl: './explore.component.scss'
 })
